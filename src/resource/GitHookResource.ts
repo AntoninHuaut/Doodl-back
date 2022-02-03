10944d25146c2925fec87ab5e5788fab491a8d3d
Antonin Huaut <antonin.huaut+github@gmail.com>
2022-02-03 21:36:41 +0100

    Git hook

--- src/resource/GitHookResource.ts
+++ src/resource/GitHookResource.ts
@@ -5,16 +5,19 @@ export default class GitHookResource extends Drash.Resource {
 
     public paths = ["/githook"];
 
-    public async POST(request: Drash.Request, _response: Drash.Response): Promise<void> {
+    public POST(request: Drash.Request, response: Drash.Response): void {
         const serverGitlabToken = Deno.env.get("GITHOOK_GITLAB_TOKEN");
         if (!serverGitlabToken) return;
         const userGitlabToken = request.headers.get("X-Gitlab-Token");
 
         if (serverGitlabToken !== userGitlabToken) throw new Drash.Errors.HttpError(404);
 
-        loggerService.info("Git Hook, deploying...")
-        const p = Deno.run({ cmd: ["./deploy.sh"] });
-        const { success, code, signal } = await p.status();
-        loggerService.info(`Git Hook, ${success} - ${code} - ${signal}`);
+        response.headers.set("Accept", "application/vnd.contentful.management.v1+json");
+        setTimeout(async () => {
+            loggerService.info("Git Hook, deploying...")
+            const p = Deno.run({ cmd: ["./deploy.sh"] });
+            const { success, code, signal } = await p.status();
+            loggerService.info(`Git Hook, ${success} - ${code} - ${signal}`);
+        }, 100);
     }
 }
