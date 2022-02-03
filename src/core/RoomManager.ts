7d045f15b8c7512c27da57ea33f2df76b5eb7510
Antonin Huaut <antonin.huaut+github@gmail.com>
2022-02-03 20:49:32 +0100

    Refractor RoomId generator

--- src/core/RoomManager.ts
+++ src/core/RoomManager.ts
@@ -2,7 +2,7 @@ import { GameMode, IPlayer } from '../model/GameModel.ts';
 import { Room } from '../model/Room.ts';
 import { loggerService } from '../server.ts';
 
-const ROOM_CODE_LENGTH = 9;
+const ROOM_CODE_LENGTH = 8;
 const roomMap = new Map<string, Room>();
 
 export function createRoom(gameMode: GameMode, maxPlayer: number, roundTimeDuration: number): Room {
@@ -32,11 +32,15 @@ export function removePlayerIdToRoom(playerId: string, room: Room) {
     room.removePlayerId(playerId);
 }
 
+/**
+ * @returns string of ROOM_CODE_LENGTH length, including letters (upper) and digits
+ */
 function generateRoomId(): string {
     const roomId = Array(ROOM_CODE_LENGTH).fill('x').join('').replace(/x/g, () => {
-        let num = Math.floor(Math.random() * 52);
-        if (num > 25) num += 6
-        return String.fromCharCode(num + 65)
+        const possibleValue = ("Z".charCodeAt(0) - "A".charCodeAt(0) + 1) + ("9".charCodeAt(0) - "0".charCodeAt(0) + 1)
+        let num = Math.floor(Math.random() * possibleValue) + "0".charCodeAt(0);
+        if (num > "9".charCodeAt(0)) num += ("A".charCodeAt(0) - "9".charCodeAt(0) - 1)
+        return String.fromCharCode(num)
     });
 
     if (roomMap.has(roomId)) {
