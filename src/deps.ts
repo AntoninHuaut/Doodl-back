80ea706d59a720cab0fefe019e4f30aff0d3c0d4
Antonin Huaut <antonin.huaut@gmail.com>
2022-02-15 12:18:11 +0100

    Update dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,9 +1,9 @@
 import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 
-export * as Drash from "https://deno.land/x/drash@v2.5.1/mod.ts";
-export {DexterService} from "https://deno.land/x/drash@v2.5.1/src/services/dexter/dexter.ts";
-export {PaladinService} from "https://deno.land/x/drash@v2.5.1/src/services/paladin/paladin.ts";
-export {CORSService} from "https://deno.land/x/drash@v2.5.1/src/services/cors/cors.ts";
-export {ResponseTimeService} from "https://deno.land/x/drash@v2.5.1/src/services/response_time/response_time.ts";
+export * as Drash from "https://deno.land/x/drash@v2.5.2/mod.ts";
+export {DexterService} from "https://deno.land/x/drash@v2.5.2/src/services/dexter/dexter.ts";
+export {PaladinService} from "https://deno.land/x/drash@v2.5.2/src/services/paladin/paladin.ts";
+export {CORSService} from "https://deno.land/x/drash@v2.5.2/src/services/cors/cors.ts";
+export {ResponseTimeService} from "https://deno.land/x/drash@v2.5.2/src/services/response_time/response_time.ts";
 export * as unilogger from "https://deno.land/x/unilogger@v1.0.4/mod.ts";
 export {z} from "https://deno.land/x/zod@v3.11.6/index.ts";
